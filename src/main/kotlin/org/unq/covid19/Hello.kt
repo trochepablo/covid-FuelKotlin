0feeb689aeb5cd5bd3682062d05f5f36360cfe59
Pablo Troche <trochepablodaniel@gmail.com>
2020-05-29 19:12:30 -0300

    init project

---
+++ src/main/kotlin/org/unq/covid19/Hello.kt
@@ -0,0 +1,6 @@
+package org.unq.covid19
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/org/unq/covid19/TestApiCOVID19.kt
@@ -0,0 +1,38 @@
+package org.unq.covid19
+
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
+import org.junit.jupiter.api.*
+import com.github.kittinunf.fuel.core.FuelManager
+import org.junit.Assert.assertEquals
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class TestApiCOVID19 {
+
+    @BeforeAll
+    fun setUp() {
+
+        FuelManager.instance.basePath = "https://covid-193.p.rapidapi.com"
+        FuelManager.instance.baseHeaders = mapOf("x-rapidapi-host" to "covid-193.p.rapidapi.com",
+            "x-rapidapi-key" to "bc915823d8mshc08e34940d1ec72p191869jsn08922a187a7d", "useQueryString" to "true")
+    }
+
+    @Test
+    @Order(1)
+    fun `get statistics`() {
+        try {
+            val (_, response, result) =
+                Fuel.get("/statistics").response()
+            println(response.data.toString(Charsets.UTF_8))
+            assertEquals(200, response.statusCode)
+        } catch (e: FuelError){
+            println(e.message)
+        }
+
+    }
+
+    @AfterAll
+    fun tearDown() {
+    }
+}
